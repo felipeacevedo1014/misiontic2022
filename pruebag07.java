21ff598b6bded0ab8340c483862aeb664dff4861
felipeacevedo1014 <89332247+felipeacevedo1014@users.noreply.github.com>
2021-08-28 20:14:49 +0900

    version1.2

--- pruebag07.java
+++ pruebag07.java
@@ -1,6 +1,6 @@
 public class prueba07{
     public static void main(String[] args) {
         System.out.println("Bienvenidos a GitHub");
-        
+        System.out.println("Mision TIC 2022");
     }
 }
